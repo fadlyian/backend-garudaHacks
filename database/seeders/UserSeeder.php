cf6e3e46ec9fa2a7ffd0d1a40bf19a8565deee57
fadlyian <mansyuraw24@gmail.com>
2024-07-12 17:17:42 +0700

    #feat create role and assign to user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,10 +2,14 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
 
 class UserSeeder extends Seeder
 {
@@ -14,26 +18,42 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
+
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // $role1 = Role::create(['name' => 'admin']);
+        $role2 = Role::create(['name' => 'company']);
+        $role3 = Role::create(['name' => 'user']);
+
+        $user1 = User::create([
             'name' => 'ian',
             'email' => 'ian@gmail.com',
             'password' => Hash::make('password'),
         ]);
-        DB::table('users')->insert([
+
+        $user1->assignRole($role2);
+
+        $user2 = User::create([
             'name' => 'adam',
             'email' => 'adam@gmail.com',
             'password' => Hash::make('password'),
         ]);
-        DB::table('users')->insert([
+        $user2->assignRole($role2);
+
+        $user3 = User::create([
             'name' => 'wili',
             'email' => 'wili@gmail.com',
             'password' => Hash::make('password'),
         ]);
-        DB::table('users')->insert([
+        $user3->assignRole($role3);
+
+        $user4 = User::create([
             'name' => 'devan',
             'email' => 'devan@gmail.com',
             'password' => Hash::make('password'),
         ]);
+        $user4->assignRole($role3);
+
 
     }
 }
